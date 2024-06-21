9fe721ddd97980ea361a3131c7642aa38562a55a
hafm919 <90020521+hafm919@users.noreply.github.com>
2024-06-21 22:38:32 +0530

    add gamelogic to UI

--- scripts/game.js
+++ scripts/game.js
@@ -1,47 +1,41 @@
-let humanScore=0;
+let playerScore=0;
 let computerScore=0;
+let roundNo=1;
+
 function getComputerChoice(){
     let choice=Math.ceil(Math.random()*3);
     
-    if (choice===1) return 'Rock';
-    else if(choice===2) return 'Paper';
-    else return 'Scissors';
+    if (choice===1) return 'shuriken';
+    else if(choice===2) return 'shield';
+    else return 'katana';
 }
 
-function getHumanChoice(){
-    let choice=parseInt(prompt("1:Rock, 2: Paper, 3:Scissors"));
-    if (choice===1) return 'Rock';
-    else if(choice===2) return 'Paper';
-    else return 'Scissors';
 
-}
-
-function playRound(){
-    let humanChoice=getHumanChoice();
+function playRound(humanChoice){
     let computerChoice=getComputerChoice();
-    console.log(computerChoice);
+    computerChoiceDisplay.style.content=`url(img/${computerChoice}.png)`;
     let winner=-1; //0:computer 1:player
-    if (humanChoice==='Rock'){
-        if(computerChoice==='Paper'){
+    if (humanChoice==='shuriken'){
+        if(computerChoice==='shield'){
             winner=0
         }
-        else if (computerChoice==='Scissors'){
+        else if (computerChoice==='katana'){
             winner=1
         }
     }
-    else if (humanChoice==='Paper'){
-        if(computerChoice==='Scissors'){
+    else if (humanChoice==='shield'){
+        if(computerChoice==='katana'){
             winner=0
         }
-        else if (computerChoice==='Rock'){
+        else if (computerChoice==='shuriken'){
             winner=1
         }
     }
-    else if (humanChoice==='Scissors'){
-        if(computerChoice==='Rock'){
+    else if (humanChoice==='katana'){
+        if(computerChoice==='shuriken'){
             winner=0
         }
-        else if (computerChoice==='Paper'){
+        else if (computerChoice==='shield'){
             winner=1
         }
     }
@@ -49,10 +43,12 @@ function playRound(){
     if(winner===0){
         console.log('Computer won')
         computerScore+=1
+        computerScoreDisplay.textContent=computerScore;
     }
     else if(winner===1){
         console.log('Human won')
-        humanScore+=1
+        playerScore+=1
+        playerScoreDisplay.textContent=playerScore;
     }
     else{
         console.log('Tie')
@@ -63,10 +59,24 @@ function playGame(){
     for(let i=0;i<5;i++){
         playRound();
     }
-    console.log("Score- Human:"+humanScore+" Computer:"+ computerScore);
-    if (humanScore>computerScore) console.log("You won!");
-    else if(humanScore<computerScore) console.log("You lost!");
+    console.log("Score- Human:"+playerScore+" Computer:"+ computerScore);
+    if (playerScore>computerScore) console.log("You won!");
+    else if(playerScore<computerScore) console.log("You lost!");
     else console.log("You tied!");
 }
 
-playGame();
+
+const playerOptions=document.querySelectorAll(".option, .player");
+const computerChoiceDisplay=document.querySelector("#computer");
+const round=document.querySelector(".round");
+const playerScoreDisplay=document.querySelector('#player-score');
+const computerScoreDisplay=document.querySelector('#computer-score');
+
+playerOptions.forEach((option)=>{
+    option.addEventListener('click',()=>{
+        let playerChoice=option.id;
+        playRound(playerChoice);
+        roundNo+=1;
+        round.textContent='Round '+roundNo;
+    });
+});
